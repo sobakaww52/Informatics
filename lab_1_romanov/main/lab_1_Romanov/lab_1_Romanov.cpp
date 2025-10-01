37e89edf15bf34ca2dd91f1d8ae8d157aa644f61
sobakaww52 <glebzhik1@gmail.com>
2025-10-01 21:02:53 +0300

    Add files via upload

---
+++ lab_1_romanov/main/lab_1_Romanov/lab_1_Romanov.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+#include <cstdlib>
+#include <cmath>
+using namespace std;
+
+int main()
+    {
+    float R, S, C;
+    float x, y;
+    cin >> x;
+    cin >> y;
+    R = sqrt(x * y) / abs(x);
+    S = cos(y) * log2(x);
+    cout << R << endl;
+    cout << S << endl;
+
+    C = max(R, S);
+    cout << C << endl;
+    system("pause");
+
+}
